0ec4c7a49c9daa88f2e5c33fb6973891a93d14f0
Lamphrangmi-Lamin <lamphrangmilamin46169@gmail.com>
2025-01-25 21:00:11 +0530

    adding eventlistener to refresh button and fixing bug in generate button

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const userInput = document.getElementById('user-input');
 const qrCodeContainer = document.getElementById('qrcode');
 const generateQrCodeBtn = document.getElementById('generate-qr-code-btn');
+const refreshQrCodeBtn = document.getElementById('refresh-qr-code-btn');
+let qrCode;
 
 
+// creating a function using the qrcode.js library to generate a qr code instance
 const generateQRCode = () => {
     const text = userInput.value;
     console.log(text);
     console.log("Generating QR code...")
-    const qrCode = new QRCode(qrCodeContainer, {
+    qrCode = new QRCode(qrCodeContainer, {
         text: text,
         width: 325,
         height: 325,
@@ -18,4 +21,17 @@ const generateQRCode = () => {
     console.log("Success...")
 }
 
-generateQrCodeBtn.addEventListener('click', generateQRCode);
+// adding functionality to the generate button
+generateQrCodeBtn.addEventListener('click', () => {
+    if (!qrCode) {
+        generateQRCode();
+    }
+});
+
+// adding functionality to the refresh button
+refreshQrCodeBtn.addEventListener('click', () => {
+    qrCode.clear();
+    qrCodeContainer.innerHTML = '';
+    qrCode = null;
+    console.log("CLEARED");
+})
